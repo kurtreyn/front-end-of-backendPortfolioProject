44954e97970f8a9c4a5adc6409895c9b918da793
KurtReyn <kurtreyn@yahoo.com>
2022-03-14 00:24:37 -0400

    posts now pulling from server/db

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,10 +1,51 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Navigation from './Navigation';
 import PostScreen from './PostScreen';
 import LoadingScreen from './LoadingScreen';
+import axios from 'axios';
 
 function HomePage() {
   const [loading, setLoading] = useState(false);
+  const [posts, setPosts] = useState([]);
+
+  const fetchPosts = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch('http://localhost:3000/posts');
+      // console.log(response);
+      let data = await response.json();
+      console.log(data);
+      setPosts(data);
+
+      // posts ? setLoading(false) : setLoading(true);
+      // console.log(posts.length);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  posts.map((item) => {
+    console.log(item.title);
+  });
+
+  // useEffect(() => {
+  //   axios.get('http://localhost:3000/posts').then(function (response) {
+  //     // handle success
+  //     console.log(response.data);
+  //     setPosts(response.data);
+  //   });
+
+  //   // .then((data) => setPosts(data.title));
+  // }, []);
+
+  // console.log(`posts are: ${posts}`);
+
+  // console.log(`loading is: ${loading}`);
 
   return (
     <>
@@ -12,12 +53,8 @@ function HomePage() {
         <Navigation />
         <div className="row post-row">
           <h1>Video Game Database</h1>
-
-          {loading ? (
-            <LoadingScreen />
-          ) : (
-            <PostScreen loading={loading} setLoading={setLoading} />
-          )}
+          {/* <PostScreen posts={posts} /> */}
+          {loading ? <LoadingScreen /> : <PostScreen posts={posts} />}
         </div>
       </div>
     </>
--- src/components/PostScreen.js
+++ src/components/PostScreen.js
@@ -1,34 +1,14 @@
 import React, { useState, useEffect } from 'react';
 
-function PostScreen({ loading, setLoading }) {
-  const [posts, setPosts] = useState(null);
-
-  const fetchPosts = async () => {
-    setLoading(true);
-    try {
-      const response = await fetch('http://localhost:3000/posts');
-      console.log(response);
-      let data = await response.json();
-      console.log(data);
-      setPosts(data);
-      posts ? setLoading(false) : setLoading(true);
-      console.log(posts.length);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  useEffect(() => {
-    fetchPosts();
-  }, []);
-
-  console.log(`loading is: ${loading}`);
-
+function PostScreen({ posts }) {
+  // posts.map((item) => {
+  //   console.log(item.title);
+  // });
   return posts.map((post) => {
     return (
       <>
         <div
-          key={post.id}
+          key={post._id}
           className="col-12 col-sm-12 col-md-3 col-lg-3 col-xl-3 post-img-col"
         >
           <h2>{post.title}</h2>
